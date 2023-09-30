5fade3d01b0f6f1968ce7edd3a3bf7d7c08366ae
AnuOuseph <anuouseph04@gmail.com>
2023-09-30 23:50:55 +0530

    Login Api connected

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,46 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom"
+import axios from 'axios';
 
 
 function Login() {
     const [reg,setReg] = useState(false)
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const [data,setData] = useState(
+        JSON.parse(localStorage.getItem("user")) || null
+    )
+    const [error,setError] = useState(null)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+    console.log(formData)
+    useEffect(() => {
+        if(data){
+            navigate('/');
+        }
+    },[data, navigate]);
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try{
+            const res = await axios.post("http://localhost:5000/api/auth/login",formData)
+            localStorage.setItem("user",JSON.stringify(res.data))
+            setData(res.data)
+            navigate('/')
+        }catch(err){
+            setError(err)
+        }
+    };
   return (
     <div className='h-screen flex justify-center items-center px-40 py-8'> 
         <div className='bg-white flex flex-col p-6 border shadow-md shadow-gray-400'>
@@ -14,12 +48,15 @@ function Login() {
             <div className='md:w-58 p-6 bg-white flex flex-col justify-center text-center items-center'>
                 <p className='text-xl font-medium mb-4'>Login to your account!</p>
                 {reg ? 
+                <>
                 <div className=' border mt-10 mb-2 border-gray-400 rounded-full w-64 h-12 bg-white flex'>
-                    <input className='w-64  px-6 py-2 rounded-full outline-none text-xs' type="password" placeholder='enter your password..' />
-                    <div className='h-10 w-10 px-4 bg-blue-500 rounded-full flex items-center justify-center transform translate-x-[-6px] translate-y-[4px]'><button onClick={()=>setReg(false)}><FontAwesomeIcon icon={faArrowRight} /></button></div>
-                </div>:
+                    <input className='w-64  px-6 py-2 rounded-full outline-none text-xs' type="password" name="password" value={formData.password} onChange={handleChange} placeholder='enter your password..' />
+                    <div className='h-10 w-10 px-4 bg-blue-500 rounded-full flex items-center justify-center transform translate-x-[-6px] translate-y-[4px]'><button onClick={handleSubmit}><FontAwesomeIcon icon={faArrowRight} /></button></div>
+                </div>
+                </>
+                :
                 <div className=' border mt-10 mb-2 border-gray-400 rounded-full w-64 h-12 bg-white flex'>
-                    <input className='w-64  px-6 py-2 rounded-full outline-none text-xs ' type="email" placeholder='enter your email address..' />
+                    <input className='w-64  px-6 py-2 rounded-full outline-none text-xs ' type="email" name="email" value={formData.email} onChange={handleChange} placeholder='enter your email address..' />
                     <div className='h-10 w-10 px-4 bg-blue-500 rounded-full flex items-center justify-center transform translate-x-[-6px] translate-y-[4px]'><button onClick={()=>setReg(true)}><FontAwesomeIcon icon={faArrowRight} /></button></div>
                 </div>}
                 <p className='text-xs'>Or</p>
